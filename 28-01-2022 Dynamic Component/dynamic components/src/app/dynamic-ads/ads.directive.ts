b3d8dd41d9e29eef88fc8de7ca3846f6a4dd783d
AjithJK2309 <ajith@nextazy.com>
2022-03-14 16:44:03 +0530

    backup

--- 28-01-2022 Dynamic Component/dynamic components/src/app/dynamic-ads/ads.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive, ViewContainerRef } from "@angular/core";
-
-@Directive ({
-    selector : '[adsDynamic]'
-})
-export class AdsDynamic {
-   
-    constructor(public vcr:ViewContainerRef){}
-}
